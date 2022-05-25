d1d2d812f2f2f7bb2430ea9a8e1948722b14b910
Lars Palm <larspalm97@gmail.com>
2022-05-25 21:50:45 +0200

    init valutaserver i java

--- valutakurs_project/ValutaServer/src/main/java/com/palm/ValutaServer/GetStoredData.java
+++ valutakurs_project/ValutaServer/src/main/java/com/palm/ValutaServer/GetStoredData.java
@@ -3,10 +3,19 @@ package com.palm.ValutaServer;
 import com.google.gson.Gson;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class GetStoredData{
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public GetStoredData(){}
+
+    public Map<String, Double> getNewestValues(){
+        String datoQuery = "SELECT max(dato) FROM endpoints_currencyvalue";
+        String valueQuery = "SELECT * FROM endpoints_currencyvalue WHERE dato = ?";
+        return null;
+    }
 } 
--- valutakurs_project/ValutaServer/src/main/java/com/palm/ValutaServer/StoreData.java
+++ valutakurs_project/ValutaServer/src/main/java/com/palm/ValutaServer/StoreData.java
@@ -3,6 +3,8 @@ package com.palm.ValutaServer;
 import com.google.gson.Gson;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class StoreData{
     @Autowired
